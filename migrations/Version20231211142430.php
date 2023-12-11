064a0e59c9a3685d890b3238ce6b920f772c9ba2
James Roe <58564719+james-webdev@users.noreply.github.com>
2023-12-11 15:30:12 +0000

    modify migration

--- migrations/Version20231211142430.php
+++ migrations/Version20231211142430.php
@@ -20,12 +20,12 @@ final class Version20231211142430 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE splap');
+        $this->addSql('CREATE TABLE splap_savings_goal (id INT AUTO_INCREMENT NOT NULL, goal INT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE splap (id INT AUTO_INCREMENT NOT NULL, goal INT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('DROP TABLE splap_savings_goal');
     }
 }
